b14e88b653e3918fd046d7ca6d6f01b248d64410
ZarArt1337 <132918164+ZarArt1337@users.noreply.github.com>
2023-07-27 13:24:35 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php ob_start();
 
-$db['db_host'] = "zarartd722.mysql.db";
-$db['db_user'] = "zarartd722";
-$db['db_pass'] = "ZarArt1337";
-$db['db_name'] = "zarartd722";
+$db['db_host'] = "??";
+$db['db_user'] = "??";
+$db['db_pass'] = "??";
+$db['db_name'] = "??";
 
 foreach($db as $key => $value){
 define(strtoupper($key), $value);
@@ -11,14 +11,7 @@ define(strtoupper($key), $value);
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-
-
 $query = "SET NAMES utf8";
 mysqli_query($connection,$query);
-
-//$connection = mysqli_connect('localhost','root','','cms');
-
-
-
 
 ?>
